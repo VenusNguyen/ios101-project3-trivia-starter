38a89153ba0be96ef5cd1621202ec930caa2f0ac
Venus Nguyen <letn6@uci.edu>
2025-03-14 23:15:31 -0700

    Complete Required Features

---
+++ Trivia/ScoreViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreViewController.swift
+//  Trivia
+//
+//  Created by Venus Nguyen on 3/14/25.
+//
+
+import Foundation
---
+++ Trivia/Trivia.swift
@@ -0,0 +1,8 @@
+//
+//  Trivia.swift
+//  Trivia
+//
+//  Created by Venus Nguyen on 3/14/25.
+//
+
+import Foundation
